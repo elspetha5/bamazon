12ec69e02b8d6f8e75fe2de6c3eb5eec8fbccda4
elspetha5 <elspetha@gmail.com>
2018-04-21 15:29:43 -0700

    manager challenege started

---
+++ bamazonManager.js
@@ -0,0 +1,9 @@
+// List of menu options
+    // view products for sale
+        // list all items with id, names, prices, and quantities
+    // view low inventory
+        // list all items with an inventory count lower than 5
+    // add to inventory
+        // display prompt that will let manager add more units of current items
+    // add new product
+        // add completely new product to store
